362ca58187f26033ecd211a0557759b76a97f881
miltonlundte19 <milton.lund@elev.ga.ntig.se>
2020-11-17 10:51:50 +0100

    Initial commit

---
+++ src/gissatalet.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class gissatalet {
+    public static void main(String[] args) {
+        double svar1;
+        int svar2;
+        svar1 =  Math.random()*100;
+        svar2 = (int) svar1;
+        int k = 1;
+        System.out.print(svar2);
+        while (k == 1) {
+            int gis = Integer.parseInt(JOptionPane.showInputDialog("gisa"));
+            if (svar2 == gis); {
+                k++;
+            } else if () {
+
+            }
+
+        }
+
+        }
+    }
+}
