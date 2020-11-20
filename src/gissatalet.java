7917fcee5674e3f84adbb1530432cd42b781ede7
miltonlundte19 <milton.lund@elev.ga.ntig.se>
2020-11-20 12:01:14 +0100

    11-20

--- src/gissatalet.java
+++ src/gissatalet.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class gissatalet {
     public static void main(String[] args) {
@@ -6,18 +7,47 @@ public class gissatalet {
         int svar2;
         svar1 =  Math.random()*100;
         svar2 = (int) svar1;
-        int k = 1;
+        boolean k = false;
         System.out.print(svar2);
-        while (k == 1) {
-            int gis = Integer.parseInt(JOptionPane.showInputDialog("gisa"));
-            if (svar2 == gis); {
-                k++;
-            } else if () {
-
+        int nedre = 1;
+        int upppre = 100;
+        String text = "Gisa på ett tall mellen" + nedre + "och" + upppre;
+        int gis;
+        int agis = 0;
+        String svar = "det tog" + gis + "försök att gisa" + svar2;
+        try {
+            gis = Integer.parseInt(JOptionPane.showInputDialog(text));
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "skrivit fell så gisningen blir 50");
+            gis = 50;
+        } catch (HeadlessException e) {
+            e.printStackTrace();
+        }
+        while (k ) {
+            try {
+                gis = Integer.parseInt(JOptionPane.showInputDialog(text));
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            } catch (HeadlessException e) {
+                e.printStackTrace();
             }
 
-        }
+            if (svar2 == gis) {
+                if (agis==0) {
+                    svar = "Gratis du gisade rett på första försöket!!";
+                    break;
+                }
+                agis++;
+                k = true;
 
+            }else if (gis < svar2) {
+                nedre = gis;
+                agis++;
+            } else if (gis > svar2) {
+                upppre = gis;
+                agis++;
+            }
         }
+        JOptionPane.showMessageDialog(null, svar);
     }
 }
